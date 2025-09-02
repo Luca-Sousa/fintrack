69318d8bbec9b8733f7b53d10db31207cc9157f1
Lucas de Sousa Silva <119131545+Luca-Sousa@users.noreply.github.com>
2025-09-02 15:41:32 -0300

    chore: add tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 const App = () => {
   return (
     <div>
-      <h1>Welcome to FinTrack</h1>
-      <p>Your one-stop solution for financial tracking.</p>
+      <h1 className="text-4xl font-bold text-red-500">Welcome to FinTrack</h1>
+      <p className="mt-2 text-lg text-red-900">
+        Your one-stop solution for financial tracking.
+      </p>
     </div>
   );
-}
- 
+};
+
 export default App;
